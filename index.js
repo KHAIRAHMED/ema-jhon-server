3907754d0a6866112c11b1bacc14e22c88322191
KHAIR AHMED <khairahmed234@gmail.com>
2021-11-16 15:56:59 +0600

    added port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 require('dotenv').config()
-const port = 5000
+const port = process.env.PORT || 5055
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
